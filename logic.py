01ae5ab574e4bba80806d9295ba6931376cdfc4c
Evren Ozkan <sleytr@gmail.com>
2019-01-19 18:11:33 +0100

    Introduced type annotations. Added TimerRound class

--- logic.py
+++ logic.py
@@ -1,18 +1,23 @@
 """
 
 """
-from dataclasses import dataclass
-from typing import Optional
+from dataclasses import dataclass, field
+from typing import List, Dict, Optional, Any, Union, cast, Type
 from enum import Enum
 import random
 from time import time
+from collections import defaultdict
 
 # Exceptions
+
+
 class PlayerError(Exception):
     pass
 
+
 class GameError(Exception):
     pass
+
 
 class Result(Enum):
     DRAW = 0
@@ -29,18 +34,20 @@ class Tool:
     can_crush: bool = False
     can_cut: bool = False
 
-    def encounter(self, tool: "Tool"):
-        return self.lose(tool) or self.draw(tool) or Result.WIN
+    def encounter(self, tool: "Tool") -> Result:
+        result = self.lose(tool) or self.draw(tool) or Result.WIN
+        assert result is not None
+        return result
 
-    def draw(self, tool: "Tool"):
-        if self == tool:
-            return Result.DRAW
+    def draw(self, tool: "Tool") -> Union[Result, None]:
+        return Result.DRAW if self == tool else None
 
-    def lose(self, tool: "Tool"):
-        if ((self.cuttable and tool.can_cut) or
-                (self.crushable and tool.can_crush) or
-                (self.wrappable and tool.can_wrap)):
-            return Result.LOSE
+    def lose(self, tool: "Tool") -> Union[Result, None]:
+        return Result.LOSE if (
+            (self.cuttable and tool.can_cut) or
+            (self.crushable and tool.can_crush) or
+            (self.wrappable and tool.can_wrap)
+            ) else None
 
 
 class Selection(Enum):
@@ -64,7 +71,7 @@ class Options:
     countdown: int = 3
     rounds: int = 3
 
-    def set(self, **kwargs):
+    def set(self, **kwargs: Any) -> None:
         for key, value in kwargs.items():
             try:
                 getattr(self, key)
@@ -75,45 +82,42 @@ class Options:
 
 @dataclass
 class Player:
+    selection: Optional[Selection] = field(init=False)
     name: str = 'Player'
-    tool: Tool = None
     auto_play: bool = False
 
-
-    def get_random_tool(self):
+    def get_random_selection(self) -> Selection:
         if self.auto_play:
-            self.tool = random.choice(list(Selection))
-            return self.tool
+            self.selection = random.choice(list(Selection))
+            return self.selection 
         else:
             raise PlayerError("Player did not made any choice")
 
-    def get_tool(self):
-        return (self.tool or self.get_random_tool()).value
+    def get_selected_tool(self) -> Tool:
+        return (self.selection or self.get_random_selection()).value
 
-    def set_tool(self, tool: Tool):        
-        self.tool = tool
+    def set_selection(self, selection_selection: Selection) -> None:
+        self.selection = selection_selection
 
-    def reset_tool(self):
-        self.tool = None
+    def reset_selection(self) -> None:
+        self.selection = None
 
-@dataclass
 class Round:
-    winner: Player = None
-    draw: bool = False
-    player1: Player = None
-    player2: Player = None
-    start_time: float = .0
-
-    def __init__(self, player1, player2):
+    def __init__(self, player1: Player, player2: Player, options: Options):
         self.player1 = player1
         self.player2 = player2
-        self.player1.reset_tool()
-        self.player2.reset_tool()
-        self.start_time = time()
-    
-    def finalize(self):
-        tool_of_p1 = self.player1.get_tool()
-        tool_of_p2 = self.player2.get_tool()
+        self.options = options
+        self.winner: Optional[Player] = None
+        self.draw: bool = False
+        self.player1.reset_selection()
+        self.player2.reset_selection()
+
+    def play(self, player: Player, selection: Selection) -> None:
+        player.set_selection(selection)
+
+    def finalize(self) -> None:
+        tool_of_p1 = self.player1.get_selected_tool()
+        tool_of_p2 = self.player2.get_selected_tool()
         result = tool_of_p1.encounter(tool_of_p2)
 
         if result == Result.WIN:
@@ -124,44 +128,69 @@ class Round:
             self.draw = True
 
 
+class TimerRound(Round):
+    start_time: float = .0
+    timings: defaultdict = defaultdict(float)
+
+    def __init__(self, player1: Player, player2: Player, options: Options) -> None:
+        self.start_time = time()
+        return super().__init__(player1, player2, options)
+
+    def play(self, player: Player, selection: Selection) -> None:
+        self.timings[player] = time()
+        super().play(player, selection)
+
+    # def check_timings(self):
+    #     self.timings
+
+    def finalize(self) -> None:
+        return super().finalize()
+
+
 class Game:
-    def __init__(self):
+    def __init__(self, round_type: Type[Round]):
+        self.RoundType = round_type
         self.options = Options()
         self.player = Player()
         self.cpu = Player(name="CPU", auto_play=True)
-        self.rounds = []
-        self.current_round = None
+        self.rounds: List[Round] = []
+        self.current_round: Optional[Round]
 
-    def set_options(self, **kwargs):
+    def set_options(self, **kwargs: Any) -> None:
         self.options.set(**kwargs)
 
-    def is_running(self):
+    def is_running(self) -> bool:
         return len(self.rounds) < self.options.rounds
 
-    def start_round(self):
+    def play(self, selection: Selection, player: Optional[Player] = None) -> None:
+        assert self.current_round is not None
+        self.current_round.play(player or self.player, selection)
+
+    def start_round(self) -> Round:
         if self.is_running():
-            self.current_round = Round(player1=self.player, player2=self.cpu)
+            self.current_round = self.RoundType(
+                player1=self.player, player2=self.cpu, options=self.options)
+            return self.current_round
         else:
             raise GameError("Game already finished")
-        
-    
-    def finish_round(self):
+
+    def finish_round(self) -> Round:
+        assert self.current_round is not None
         finished_round = self.current_round
-        self.current_round.finalize()
-        self.rounds.append(self.current_round)
+        finished_round.finalize()
+        self.rounds.append(finished_round)
         self.current_round = None
         return finished_round
 
-    def get_number_of_wins(self, player: Player):
+    def get_number_of_wins(self, player: Player) -> int:
         return sum([1 for round in self.rounds if round.winner == player])
 
-    def get_result(self):
+    def get_result(self) -> Result:
         player_won = self.get_number_of_wins(self.player)
-        cpu_won =  self.get_number_of_wins(self.cpu)
+        cpu_won = self.get_number_of_wins(self.cpu)
         if player_won > cpu_won:
             return Result.WIN
         elif player_won == cpu_won:
             return Result.DRAW
         else:
             return Result.LOSE
-        
--- tests/test_game_logic.py
+++ tests/test_game_logic.py
@@ -2,36 +2,36 @@ import pytest
 
 from ..logic import *
 
-def test_options():
+def test_options() -> None:
     option = Options()
     assert option.countdown == Options.countdown
     option.countdown = 1
     assert option.countdown == 1
-    option.set(rounds=1, countdown=4)
+    option.set(rounds=1, countdown=4)    
     assert option.countdown == 4
     assert option.rounds == 1
     with pytest.raises(AttributeError):
         option.set(foo=1)
         
 
-def test_game_play():
-    game = Game()
+def test_game_play() -> None:
+    game = Game(Round)
     game.start_round()    
-    game.player.set_tool(Selection.PAPER)
-    game.cpu.set_tool(Selection.PAPER)
+    game.play(Selection.PAPER)        
+    game.play(Selection.PAPER, game.cpu)
     finished_round = game.finish_round()
     assert finished_round.winner == None
     assert finished_round.draw == True
     game.start_round()    
-    game.player.set_tool(Selection.PAPER)
-    game.cpu.set_tool(Selection.ROCK)
-    finished_round = game.finish_round()
+    game.play(Selection.PAPER)
+    game.play(Selection.ROCK, game.cpu)
+    finished_round = game.finish_round()    
     assert finished_round.winner == game.player
     game.start_round()    
     with pytest.raises(PlayerError):
         game.finish_round()
-    game.player.set_tool(Selection.SCISSORS)
-    game.cpu.set_tool(Selection.ROCK)
+    game.play(Selection.SCISSORS)
+    game.play(Selection.ROCK, game.cpu)
     finished_round = game.finish_round()
     assert finished_round.winner == game.cpu
     assert game.get_result() == Result.DRAW
